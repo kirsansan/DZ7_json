c1cb788e278b6a85c9f6c21cdf362977b9ddc3ee
kirsansan <kirill_home@mail.ru>
2023-02-05 17:00:42 +0300

    add decorator with parameters

--- jdata/utils.py
+++ jdata/utils.py
@@ -5,7 +5,36 @@ import os
 from pprint import pprint
 
 
-def load_from_json_file(filename: str ='./students.json') -> dict:
+def print_where_we_going_from(tell_me_about_you):
+    """
+    decorator with parameter
+    :param tell_me_about_you: I guess to see where are we going from (main or module)
+    :return: link to real_decorator func
+    """
+
+    def real_decorator(function):
+        """
+        I am real decorator but I can use parameter from above decorator
+        :param function: our function which we need to decorate
+        :return: link to wrapper
+        """
+
+        # print("я вижу что ты пришел из", tell_me_about_you)
+        def wrapper(*args):
+            """ I  only decorator inside other decorator. I going to wrap your function"""
+            print("я вижу что ты пришел из", tell_me_about_you)
+            print("будем читать файл", *args)
+            res = function(*args)  # it's our function with they arguments, and it must be returned
+            print("прочитано")
+            return res
+
+        return wrapper
+
+    return real_decorator
+
+
+@print_where_we_going_from(__name__)
+def load_from_json_file(filename: str = './students.json') -> dict:
     """ load any information from json-format file and return it"""
 
     # if we need load from extermal site
@@ -14,17 +43,18 @@ def load_from_json_file(filename: str ='./students.json') -> dict:
 
     # load from file
     if not os.path.exists(filename):
-        return {}   # we did not find file and need to return nothing
-                    # seriously we need to create ecxeption
+        return {}  # we did not find file and need to return nothing
+        # seriously we need to create ecxeption
     with open(filename, 'r', encoding='utf-8') as fh:  # open file
         data = json.load(fh)  # load data
 
-    fh.close()   # I know that "with" has already close file. It is a control bullet only =)
+    fh.close()  # I know that "with" has already close file. It is a control bullet only =)
     # at this point we can decipher data and put to my structure
     # but in first version we will return all data 'as is'
     return data
 
-def get_student_by_pk(pk: int, data: dict = {} ) -> dict:
+
+def get_student_by_pk(pk: int, data: dict = {}) -> dict:
     """ return dict by the number of record
         return include fields:
         'pk' - str;
@@ -36,13 +66,15 @@ def get_student_by_pk(pk: int, data: dict = {} ) -> dict:
             return challanger
     return None  # we can't find anything
 
+
 def get_profession_by_title(title: str, data: dict = {}) -> list:
     """ return list of skills-strings  by the string of title"""
     for challanger in data:
         if challanger['title'] == title:
             return challanger['skills']
-    print("kak ya suda popal?")     # this string will print instead exeption
+    print("kak ya suda popal?")  # this string will print instead exeption
     return None
+
 
 def check_fitness(student: dict, profession: list) -> dict:
     """ compare student by professions
@@ -59,7 +91,7 @@ def check_fitness(student: dict, profession: list) -> dict:
     # print("вот скилы которые хотим найти: ", profession)
     has = list(set(student['skills']) & set(profession))
     lacks = list(set(profession) - set(student['skills']))
-    fit_percent: int = round(100*len(has)/len(profession), 0)
+    fit_percent: int = round(100 * len(has) / len(profession), 0)
     # [has.append(search_prof) for search_prof in profession if student['skills'] in profession['skills'] ]
     return {'has': has, 'lacks': lacks, "fit_percent": int(fit_percent)}
 
